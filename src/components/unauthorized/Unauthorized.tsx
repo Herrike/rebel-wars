4469b78357f218ed72aa14d850af421a497d0248
U-OMICRON\inkbo <henrique.donzelli@gmail.com>
2022-06-21 08:57:06 +0200

    more cleanups

--- src/components/unauthorized/Unauthorized.tsx
+++ src/components/unauthorized/Unauthorized.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react'
 
-const Unauthorized: FC = () => {
+const Unauthorized: FC<{ minViewport: number }> = ({ minViewport }) => {
   return (
     <div className='unauthorized'>
       <h2>[ ! ] Unauthorized</h2>
       <p>
         On behalf of the Admiral and the Princess, any attempt to use this system from terminals
-        with resolution less than 1024 is prohibited and punished by banishment from the Alliance.
+        with resolution less than {minViewport} is prohibited and punished by banishment from the
+        Alliance.
       </p>
     </div>
   )
