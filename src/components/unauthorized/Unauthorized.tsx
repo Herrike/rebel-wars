9d7131a9413a4df1788e59a3b9e122c05305720f
U-OMICRON\inkbo <henrique.donzelli@gmail.com>
2022-06-21 08:12:52 +0200

    unauthorized paragraph re-wording

--- src/components/unauthorized/Unauthorized.tsx
+++ src/components/unauthorized/Unauthorized.tsx
@@ -6,7 +6,7 @@ const Unauthorized: FC = () => {
       <h2>[ ! ] Unauthorized</h2>
       <p>
         On behalf of the Admiral and the Princess, any attempt to use this system from terminals
-        with resolution less than 1024 is prohibited and punished by banishment.
+        with resolution less than 1024 is prohibited and punished by banishment from the Alliance.
       </p>
     </div>
   )
